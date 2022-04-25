6e05b28119c8b3326900b0dfd388714b9021c2cf
Alexis <alexis.acevedo@outlook.com>
2022-04-25 19:59:18 -0300

    CORS

--- main.py
+++ main.py
@@ -7,7 +7,20 @@ from fastapi.middleware.cors import CORSMiddleware
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.add_middleware(CORSMiddleware(allow_origins=["*"], allow_methods=["*"], ))
+origins = [
+    "http://localhost.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(router, prefix="/users", tags=["users"])
 
