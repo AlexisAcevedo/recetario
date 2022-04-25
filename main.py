95f6eb75f8cd1cef7cd6a2d827e3369cb7e417a4
Alexis <alexis.acevedo@outlook.com>
2022-04-25 19:03:39 -0300

    Added CORS

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from fastapi import FastAPI
 import models
 from router import router
 from config import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware(allow_origins=["*"], allow_methods=["*"], ))
 
 app.include_router(router, prefix="/users", tags=["users"])
 
 
 
 #manejar los distintos codigos de error
-#devolver user en los response
+
